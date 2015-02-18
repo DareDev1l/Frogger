1c035f7dc2a74c29cd1d12269ae6943c8a30c246
Radostin <angelovradostin@gmail.com>
2015-02-18 17:11:08 +0200

    Added some basic classes

---
+++ Frogger/Frogger/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frogger
+{
+    class Car
+    {
+        private int x = Console.BufferWidth / 2;
+        private int y = Console.BufferHeight;
+
+        Car Car
+        {
+
+        }
+
+        public int X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+
+        public int Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
---
+++ Frogger/Frogger/Engine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frogger
+{
+    //here the game starts and ends, here we call all other stuff
+    class Engine
+    {
+
+        static void Main()
+        {
+            Console.WindowWidth = 51;
+            Console.WindowHeight = 99;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+
+            while (true)
+            {
+                
+            }
+            
+        }
+    }
+}
