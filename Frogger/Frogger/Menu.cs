6f5a1c72add3649a0ebf9ec836ebd525588103f0
Krasimir <krasimir.stoyanov5@gmail.com>
2015-02-26 17:20:11 +0200

    Added Unfunctional menu

---
+++ Frogger/Frogger/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frogger
+{
+    class Menu
+    {
+        static void DrawMenu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Start Game");
+            Console.SetCursorPosition(20,20);
+            Console.WriteLine("PROJECT BY GROUP SERPENT FLY");
+            Console.WriteLine();
+        }
+    }
+}
